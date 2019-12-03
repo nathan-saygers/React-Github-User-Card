e466d93dcbb9f9c1dcf84c5bd85e7bc8df039503
nathan-saygers <nathan.saygers@gmail.com>
2019-12-03 12:50:30 -0800

    rethought structure and added new components

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      user: {},
+      userFollowers: [],
+    }
+  }
+
+  componentDidMount() {
+    axios.get('https://api.github.com/users/nathan-saygers')
+    .then(response => {
+      this.setState({user: response.data})
+      console.log('this is state', this.state.user)
+    })
+  }
+  
 
   render () {
     return (
